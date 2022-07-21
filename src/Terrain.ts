b054960eda9aa8c7a4748257591d9957529bf2c3
feng <908087098@qq.com>
2022-07-21 20:52:58 +0800

    @feng3d/terrain

--- src/Terrain.ts
+++ src/Terrain.ts
@@ -1,51 +1,53 @@
-namespace feng3d
+import { RegisterComponent, Renderable, Geometry, Material, GameObject, createNodeMenu } from '@feng3d/core';
+import { TerrainData } from './TerrainData';
+
+declare global
 {
-
-    export interface ComponentMap { Terrain: Terrain }
-
-    /**
-     * The Terrain component renders the terrain.
-     */
-    // @ov({ component: "OVTerrain" })
-    @RegisterComponent()
-    export class Terrain extends Renderable
+    export interface MixinsComponentMap
     {
-        __class__: "feng3d.Terrain";
-
-        /**
-         * 地形资源
-         */
-        assign: TerrainData;
-
-        geometry = Geometry.getDefault("Terrain-Geometry");
-
-        constructor()
-        {
-            super();
-            this.material = Material.getDefault("Terrain-Material");
-        }
+        Terrain: Terrain
     }
 
-    GameObject.registerPrimitive("Terrain", (g) =>
-    {
-        g.addComponent(Terrain);
-    });
-
-    export interface PrimitiveGameObject
+    export interface MixinsPrimitiveGameObject
     {
         Terrain: GameObject;
     }
-
-    // 在 Hierarchy 界面新增右键菜单项
-    createNodeMenu.push(
-        {
-            path: "3D Object/Terrain",
-            priority: -20000,
-            click: () =>
-            {
-                return GameObject.createPrimitive("Terrain");
-            }
-        }
-    );
-
 }
+
+/**
+ * The Terrain component renders the terrain.
+ */
+// @ov({ component: "OVTerrain" })
+@RegisterComponent()
+export class Terrain extends Renderable
+{
+    __class__: 'feng3d.Terrain';
+
+    /**
+     * 地形资源
+     */
+    assign: TerrainData;
+
+    geometry = Geometry.getDefault('Terrain-Geometry');
+
+    constructor()
+    {
+        super();
+        this.material = Material.getDefault('Terrain-Material');
+    }
+}
+
+GameObject.registerPrimitive('Terrain', (g) =>
+{
+    g.addComponent(Terrain);
+});
+
+// 在 Hierarchy 界面新增右键菜单项
+createNodeMenu.push(
+    {
+        path: '3D Object/Terrain',
+        priority: -20000,
+        click: () =>
+            GameObject.createPrimitive('Terrain')
+    }
+);
